306536d579d2a0ed45ca7a83470b7cf73a867897
Himesh Kothari <hkothari@slb.com>
2023-01-17 22:18:44 +0530

    update logic

--- start.sh
+++ start.sh
@@ -6,24 +6,24 @@ set -e
 echo "$PWD"
 ls
 
-mkdir "$PWD/probe-deployment/files/dbs"
+# -p for creating if not present and ignore if exists
+mkdir -p "$PWD/probe-deployment/files/dbs"
 export PROMETHEUS_MULTIPROC_DIR="$PWD/probe-deployment/files/dbs"
 
 ## Processes needs to be multi threaded because we have to keep server up and running when we run
 python3 /app/probe-deployment/files/exporter.py &
 
 while [ True ]; do
-    echo "Check directory"
-    if [ -z "$(ls -A $PWD/probe-deployment/files/dbs)" ]; then
-        echo "DBs directory is empty"
-    else
-        echo "DBs directory is not empty"
-        rm -rf $PWD/probe-deployment/files/dbs/
-        mkdir -p "$PWD/probe-deployment/files/dbs"
-    fi
     echo "Starting Tests"
     npx wdio wdio.conf.js --suite PROBE || {
         echo "Tests Failed !!!"
     }
     sleep 1000
 done
+
+while sudo fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1; do
+    echo "Waiting to get lock /var/lib/dpkg/lock-frontend..."
+    sleep 5
+done
+
+sudo apt-get update
